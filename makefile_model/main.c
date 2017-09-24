4f0455e571a39c703f0a9875fe1012937c135934
yaxinsn <yaxinsn@163.com>
2017-09-24 13:41:00 +0800

    add makefile_model

---
+++ makefile_model/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+//#include "int.h"
+//#include "intd.h"
+#include <assert.h>
+
+
+#include "libadd.h"
+typedef int int32_t;
+
+int main(void)
+{
+	int32_t a=3;
+	assert(a!=3);
+	printf("-------------\n");
+	return a;
+
+}
