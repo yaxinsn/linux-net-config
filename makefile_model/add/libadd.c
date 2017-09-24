df427bbdecb14964ce5f7069102a50979eb103dd
yaxinsn <yaxinsn@163.com>
2017-09-24 16:00:52 +0800

    mkdir add dir

---
+++ makefile_model/add/libadd.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include "libadd.h"
+
+
+int add(int a,int b)
+{
+	return a+b;
+}
--- makefile_model/main.c
+++ makefile_model/main.c
@@ -12,6 +12,7 @@ int main(void)
 {
 	int32_t a=3;
 	assert(a!=3);
+	add(2,3);
 	printf("-------------\n");
 	return a;
 
