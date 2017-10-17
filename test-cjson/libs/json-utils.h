0b054e109abd0b50a1a148b344529b84651acbf7
yaxinsn <yaxinsn@163.com>
2017-10-16 20:41:46 -0700

    add libs

---
+++ test-cjson/libs/json-utils.h
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "json-c/json.h"
+
+json_object* create_sjon_from_string(char* str);
+const char* find_value_from_sjon_by_key(json_object* obj,char* skey);
+
+void free_json(json_object* obj);
+char* find_value_from_sjon_by_key2(json_object* obj,char* skey);
+const json_object* find_value_from_json_object_by_key(json_object* obj,char* skey);
