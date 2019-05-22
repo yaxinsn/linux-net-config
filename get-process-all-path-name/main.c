be9e5a38f28f9457a0fbf1f027f3b8920f0563c5
root <root@localhost.localdomain>
2019-05-22 17:07:16 +0800

    增加一个取father路径的函数

--- get-process-all-path-name/main.c
+++ get-process-all-path-name/main.c
@@ -1,37 +1,71 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <strings.h>
+
+
 #define MAXPATH 1024
-
-#if 0
-int get_all_path(const char* fileName,char* ret_path,const int size)
-{
-
-}
-#endif
-
-
 //获取当前程序绝对路径
-int get_Self_absolute_path(char* ret_path,const int size)
+int get_self_absolute_path(char* ret_path,const int size)
 {
         int cnt = readlink("/proc/self/exe", ret_path, size);
         if(cnt >0)
         {
-                return 0;
+			return 0;
         }
         else
-                return -1;
+		{
+			return -1;
+		}
+		
 }
+
+int get_self_absolute_father_path(char* ret_path,const int size)
+{
+	int iRet;
+	char* p = NULL;
+	iRet = get_self_absolute_path(ret_path,size);
+	if(iRet == 0)
+	{
+		p = rindex(ret_path,'/');
+		if(p == NULL)
+		{
+			return -1;
+		}
+		else
+		{
+#if 0
+			if(p != ret_path)
+			{
+				*p = 0;
+			}
+#else
+			*(p+1) = 0;		
+#endif			
+			return 0;
+		}
+	}
+		
+}
+
+
 int main(void)
 {
 
 
 
- char buffer[MAXPATH];
-if( 0 ==get_Self_absolute_path(buffer,1024))
+char buffer[MAXPATH] = {0};
+if( 0 ==get_self_absolute_path(buffer,1024))
 {
  printf("The current process's file :%s\n",buffer);
 }
+
+
+if( 0 ==get_self_absolute_father_path(buffer,1024))
+{
+ printf("The current process's file :%s\n",buffer);
+}
+
  return 0;
 }
 
